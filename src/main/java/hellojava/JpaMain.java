54215807994c079f7fffc7b1830de7eccb484e3f
khj <natista00@naver.com>
2025-02-09 13:51:00 +0900

    hellojpa

---
+++ src/main/java/hellojava/JpaMain.java
@@ -0,0 +1,41 @@
+package hellojava;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.h2.engine.User;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            /* 입력
+            Member member = new Member();
+            member.setId(1);
+            member.setName("Josep");
+            */
+
+            /* 수정
+            * find => java의 collection처럼 객체를 저장해 주는 기능
+            * */
+            Member findMember =  em.find(Member.class, 1);
+
+            System.out.println(findMember);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+         emf.close();
+
+    }
+}
---
+++ src/main/java/hellojava/Member.java
@@ -0,0 +1,30 @@
+package hellojava;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Users")
+public class Member {
+
+    @Id
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
